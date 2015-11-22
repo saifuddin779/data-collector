144a7c64d2010ba4dbbee9b30ab9277f0da025c9
Saifuddin Abdullah <saif.778@gmail.com>
2015-11-22 03:15:34 -0500

    ..

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo y | apt-get install python-lxml
 
 #echo y | apt-get install autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev
 #echo y | apt-get install python-dateutil python-docutils python-feedparser python-gdata python-jinja2 python-ldap python-libxslt1 python-lxml python-mako python-mock python-openid python-psycopg2 python-psutil python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-unittest2 python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi
-#echo y | apt-get install python-pip
+echo y | apt-get install python-pip
 
 git config --global user.name "saifuddin778"
 git config --global user.email "saif.778@gmail.com"
