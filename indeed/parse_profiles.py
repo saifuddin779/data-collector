53fc71846ed38bb5291d64c2cc7f7a3e3839470b
Saifuddin Abdullah <saif.778@gmail.com>
2015-11-28 01:19:12 -0500

    ..

--- indeed/parse_profiles.py
+++ indeed/parse_profiles.py
@@ -147,7 +147,7 @@ class indeed_resumes(object):
 	
 	def begin(self):
 		sorts = ['sort=date', '']
-		keywords_done_idx = 45
+		keywords_done_idx = 3
 		#keywords_done_idx = self.r_master.get(self.country_code) #--this over here should talk to master's redis
 		print 'starting from %s' % str(keywords_done_idx)
 		if not keywords_done_idx:
