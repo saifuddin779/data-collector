e4e6a48b5a5e8828991ac5d963b21820cc760aae
Saifuddin Abdullah <saif.778@gmail.com>
2015-12-02 11:43:09 -0500

    another stupid typo fix

--- indeed/server.py
+++ indeed/server.py
@@ -14,9 +14,10 @@ def begin():
 	index = int(request.args['index'])
 	country_code = request.args['country_code'].encode('utf-8')
 	#command = "tmux send -t scrap_session.0 'python parse_profiles.py %s prod %d' ENTER" % (country_code, index)
-	commans = "tmux send -t scrap_session:1.0 'python parse_profiles.py %s prod %d' ENTER" % (country_code, index)
-	p = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=True)
-	return json.dumps({'status': True})
+	command = "tmux send -t scrap_session:1.0 'python parse_profiles.py %s prod %d' ENTER" % (country_code, index)
+	#p = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=True)
+	execute = call([command], shell=True)
+	return 1
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
