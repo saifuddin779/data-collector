ea024936c79dd0390b9376ff25d73acda5913d79
Saifuddin Abdullah <saif.778@gmail.com>
2015-12-06 07:01:35 -0500

    adding greedy

---
+++ indeed/greedy.py
@@ -0,0 +1,28 @@
+import sys, os
+from time import sleep
+
+from parse_profile_details import indeed_resumes_details
+
+def greedy():
+	directory = '../../data/collect/'
+	while True:
+		directory_list = os.listdir(directory)
+		if directory_list:
+			for i in directory_list:
+				f = open(directory+'%s' % i, 'rb')
+				for each in f.readlines():
+					if each:
+						obj = indeed_resumes_details(each.encode('utf-8'))
+						data = obj.resource_collection()
+						obj.save_to_disk(data, each)
+						print '-->', each
+						sleep(0.7)
+				f.close()
+				os.remove(directory+'%s' % i)
+		else:
+			print 'waiting for the dispatch..'
+			sleep(20)
+
+if __name__ == '__main__':
+	greedy()
+
